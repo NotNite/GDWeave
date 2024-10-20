a8add424e7b40482be27d5f1643cdf1db41b262e
NotNite <hi@notnite.com>
2024-10-20 13:06:55 -0400

    Delete WebfishingPlus

--- package.ps1
+++ package.ps1
@@ -1,7 +1,3 @@
-dotnet clean
-dotnet build -c Release
-cargo build --release
-
 if (Test-Path ./local/GDWeave) {
   Remove-Item ./local/GDWeave -Recurse
 }
@@ -12,13 +8,3 @@ if (Test-Path ./local/GDWeave.zip) {
   Remove-Item ./local/GDWeave.zip
 }
 Compress-Archive -Path ./local/GDWeave/* -DestinationPath ./local/GDWeave.zip
-
-if (Test-Path ./local/WebfishingPlus) {
-  Remove-Item ./local/WebfishingPlus -Recurse
-}
-Copy-Item -Path ./WebfishingPlus/bin/Release/net8.0 -Destination ./local/WebfishingPlus -Recurse
-
-if (Test-Path ./local/WebfishingPlus.zip) {
-  Remove-Item ./local/WebfishingPlus.zip
-}
-Compress-Archive -Path ./local/WebfishingPlus/* -DestinationPath ./local/WebfishingPlus.zip
