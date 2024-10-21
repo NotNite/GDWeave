6540d538f856cf230a0bcb0eb29de02cf5f1e814
NotNite <hi@notnite.com>
2024-10-20 20:27:45 -0400

    2.0.3

--- package.ps1
+++ package.ps1
@@ -4,6 +4,9 @@ if (Test-Path ./local/GDWeave) {
 Copy-Item -Path ./GDWeave/bin/Release/net8.0 -Destination ./local/GDWeave/GDWeave/core -Recurse
 Copy-Item -Path ./target/release/loader.dll -Destination ./local/GDWeave/winmm.dll
 
+New-Item -Path ./local/GDWeave/GDWeave/mods -ItemType Directory
+Write-Output "Copy your mods into this directory (make sure they're in separate folders)." > ./local/GDWeave/GDWeave/mods/README.txt
+
 if (Test-Path ./local/GDWeave.zip) {
   Remove-Item ./local/GDWeave.zip
 }
