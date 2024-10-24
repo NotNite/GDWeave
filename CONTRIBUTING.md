540e9b70305fadfe6a8e271a2e023f2b50e0513a
NotNite <hi@notnite.com>
2024-10-24 09:10:15 -0400

    FunctionWaiter: slight cleanup, update CONTRIBUTING

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -6,9 +6,13 @@ Most problems with contributions aren't hard blockers for merge, as they are usu
   - Use LF line format.
   - Use K&R braces.
   - Use file-scoped namespaces.
+  - Use `this.` for fields/properties.
   - Use `PascalCase` for namespaces, types, and public/static fields/properties. Use `camelCase` for private fields/properties. No prefixed underscore on private fields/properties.
   - Use `var` where possible.
   - Prefer fields to properties (unless in an interface). Fields can be `ref`d and are easier to deal with in the context of native code.
+  - Use `readonly` where possible.
+  - Prefer `const` over `static`.
+  - Use primary constructors where possible.
 - Keep in mind the contract for modding and API breakage.
   - Don't make changes to enums or interfaces that mods use.
   - Classes that shouldn't be exposed to mods should be `internal` or `private`.
--- GDWeave/Script/ModUtil.cs
+++ GDWeave/Script/ModUtil.cs
@@ -10,51 +10,46 @@ public interface IWaiter {
     public bool Check(Token token);
 }
 
-public class FunctionWaiter : IWaiter {
-    private MultiTokenWaiter functionWaiter;
+public class FunctionWaiter(string name, bool waitForReady = false) : IWaiter {
+    private readonly MultiTokenWaiter functionWaiter = new([
+        t => t.Type == TokenType.PrFunction,
+        t => t is IdentifierToken identifier && identifier.Name == name
+    ]);
 
-    public bool Matched {get; private set;} = false;
-    public bool Ready {get; private set;} = true;
-
-    public FunctionWaiter(string name, bool waitForReady = false) {
-        functionWaiter = new([
-            t => t.Type == TokenType.PrFunction,
-            t => t is IdentifierToken identifier && identifier.Name == name,
-        ]);
-    }
+    public bool Matched { get; private set; }
+    public bool Ready { get; private set; } = !waitForReady;
 
     public void Reset() {
-        foundFunction = false;
-        foundColon = false;
+        this.foundFunction = false;
+        this.foundColon = false;
+        this.Matched = false;
+        this.Ready = !waitForReady;
+        this.functionWaiter.Reset();
     }
 
     public void SetReady() {
-        Ready = true;
+        this.Ready = true;
     }
 
     private bool foundFunction = false;
     private bool foundColon = false;
-    public bool Check(Token token) {
-        if (!Ready) {
-            return false;
-        }
 
-        if (foundColon) {
-            Matched = true;
+    public bool Check(Token token) {
+        if (!this.Ready || this.Matched) return false;
+
+        if (this.foundColon && token.Type == TokenType.Newline) {
+            this.Matched = true;
             return true;
         }
 
-        if (foundFunction && token.Type == TokenType.Colon) {
-            foundColon = true;
+        if (this.foundFunction && token.Type == TokenType.Colon) {
+            this.foundColon = true;
             return false;
         }
 
-        if (!functionWaiter.Check(token)) {
-            foundFunction = true;
+        if (this.functionWaiter.Check(token)) {
+            this.foundFunction = true;
             return false;
-        }
-        else {
-            functionWaiter.Reset();
         }
 
         return false;
